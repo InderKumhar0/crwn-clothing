a9b88880157fd434a08e07878690e572a654b15c
InderKumhar0 <inderkumhar58@gmail.com>
2024-02-14 16:57:22 +0530

    typing cart redux

---
+++ src/store/cart/cart.reducer.ts
@@ -0,0 +1,35 @@
+import {  CartItem } from "./cart.types";
+
+import { setCartItems, setIsCartOpen } from "./cart.action";
+import { AnyAction } from "redux-saga";
+
+export type CartState = {
+  isCartOpen: boolean;
+  cartItems: CartItem[];
+};
+
+const INITIAL_STATE = {
+  isCartOpen: false,
+  cartItems: [],
+};
+
+export const cartReducer = (
+  state = INITIAL_STATE,
+  action: AnyAction
+): CartState => {
+  if (setIsCartOpen.match(action)) {
+    return {
+      ...state,
+      isCartOpen: action.payload,
+    };
+  }
+
+  if(setCartItems.match(action)){
+    return {
+      ...state,
+      cartItems: action.payload,
+    }
+  };
+
+  return state;
+}
